ec6832fe93c7109fc18f9d89c60b224d920bc0d9
Oscar Aguilar <aguilargonzalezoscar15@gmail.com>
2023-11-05 23:53:59 -0600

    disclaimer agregado

--- public/modulos/main.php
+++ public/modulos/main.php
@@ -5,20 +5,15 @@
 <div class="container text-center">
 
     <div class="row">
-        <div class="col-6 col-md-6">
-        Consultar
-        </div>
-        <div class="col-6 col-md-6">
-        Agregar
-        </div>
+        <h1>Descargo de responsabilidad:</h1>
     </div>
     <div class="row">
-        <div class="col-6 col-md-6">
-        Editar
-        </div>
-        <div class="col-6 col-md-6">
-        Eliminar usuario
-        </div>
-    </div>
+    
+
+Este sistema es un proyecto personal y se proporciona "tal cual", sin garantías de ningún tipo, expresas o implícitas. El autor no se hace responsable de cualquier uso que se le dé a este sistema. El autor no se hace responsable de cualquier daño, pérdida de datos, pérdida financiera, interrupción del servicio, o cualquier otro problema que pueda surgir del uso de este sistema.
+
+El sistema puede tener vulnerabilidades y no se garantiza su seguridad. El uso de este sistema es bajo su propio riesgo. El autor no se hace responsable de cualquier problema de seguridad que pueda surgir del uso de este sistema.
+
+Este sistema es para fines educativos y de aprendizaje. No se recomienda su uso en un entorno de producción.
 
 </div>
---
+++ readme.md
@@ -0,0 +1,3 @@
+Copiar y pegar la carpeta en un servidor WAMP en WWW
+Debe tener GD Activo con php 8
+Debes tener instalado composer
